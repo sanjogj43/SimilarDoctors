437c942c2d0d93efbd8c8a3b14fcaa5824284416
Sanjog Jadhav <sanjogj43@gmail.com>
2016-12-02 01:53:28 -0500

    Project created

---
+++ SimilarDoctors/SimilarDoctors/ListOfDoctorsViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimilarDoctors
+{
+    // Define a singleton class for List of Doctors. This way the list could not be altered from outside and
+    // we can have just one instance of this class.
+    public class ListOfDoctorsViewModel
+    {
+        // private static object of itself
+        private static ListOfDoctorsViewModel ListClassObj;
+        
+        public List<DoctorViewModel> ListOfDoctors { get; set; }
+        
+        public static ListOfDoctorsViewModel GetInstance()
+        {
+            if (ListClassObj == null)
+            {
+                ListClassObj = new ListOfDoctorsViewModel();
+                return ListClassObj;
+            }
+            else
+            {
+                return ListClassObj;
+            }
+        }
+
+        // Private constructor
+        private ListOfDoctorsViewModel()
+        {
+            ListOfDoctors = new List<DoctorViewModel>
+            {
+                new DoctorViewModel{ Id = 1,Name = "Mark Smith",Speciality="Pediatrics",AreaCode="90034",Organisation="Pacific Alliance",ReviewScore=4.5,NumberOfReviews=18,Degree="MD"},
+                new DoctorViewModel{ Id = 2,Name = "John Cooper",Speciality="Internal",AreaCode="90030",Organisation="Pacific Alliance",ReviewScore=3.5,NumberOfReviews=50,Degree="DO"},
+                new DoctorViewModel{ Id = 3,Name = "Jill Anderson",Speciality="Pediatrics",AreaCode="90029",Organisation="White Memorial",ReviewScore=5,NumberOfReviews=20,Degree="MD"},
+                new DoctorViewModel{ Id = 4,Name = "Phil Triu",Speciality="Internal",AreaCode="90013",Organisation="White Memorial",ReviewScore=2.9,NumberOfReviews=15,Degree="MD"},
+                new DoctorViewModel{ Id = 5,Name = "Rakesh Mehta",Speciality="Pediatrics",AreaCode="90034",Organisation="Pacific Alliance",ReviewScore=4,NumberOfReviews=32,Degree="DO"},
+                new DoctorViewModel{ Id = 6,Name = "Jim Summers",Speciality="Pediatrics",AreaCode="90034",Organisation="Silver Lake",ReviewScore=3.2,NumberOfReviews=22,Degree="MD"},
+                new DoctorViewModel{ Id = 7,Name = "James Harris",Speciality="Internal",AreaCode="90013",Organisation="White Memorial",ReviewScore=3.9,NumberOfReviews=7,Degree="MD"},
+                new DoctorViewModel{ Id = 8,Name = "Eric Mellum",Speciality="Geriatric",AreaCode="90029",Organisation="Pacific Alliance",ReviewScore=2.7,NumberOfReviews=9,Degree="MD"},
+                new DoctorViewModel{ Id = 9,Name = "Bill Clinton",Speciality="Pediatrics",AreaCode="90030",Organisation="White Memorial",ReviewScore=4.7,NumberOfReviews=20,Degree="PHD"},
+                new DoctorViewModel{ Id = 10,Name = "Jeff Sully",Speciality="Family",AreaCode="90013",Organisation="Pacific Alliance",ReviewScore=3.4,NumberOfReviews=23,Degree="PHD"},
+                new DoctorViewModel{ Id = 11,Name = "Nathan Drake",Speciality="Family",AreaCode="90013",Organisation="Silver Lake",ReviewScore=4.1,NumberOfReviews=18,Degree="DO"}
+            };
+        }
+
+        // Display list of doctors to the user
+        public static void DisplayListOfdoctors()
+        {
+            Console.WriteLine("Please select the id from following list of Doctors:");
+            Console.WriteLine("    1 : Mark Smith\n    2 : John Cooper\n    3 : Jill Anderson\n    4 : Phil Triu\n    5 : Rakesh Mehta");
+            Console.WriteLine("    6 : Jim Summers\n    7 : James Harris\n    8 : Eric Mellum\n    9 : Bill Clinton\n    10 : Jeff Sully\n    11 : Nathan Drake\n");
+        }
+    }
+}
---
+++ SimilarDoctors/SimilarDoctors/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimilarDoctors
+{
+    //A site contains a listing of doctors. Users can browse for doctors given a 
+    //specific specialty, area, review score etc. You need to help write the logic 
+    //for returning similar doctors.  Specifically, write a class(es) which when 
+    //given a doctor, provides a list of similar doctors, in a prioritized order. 
+    //You define what similar means and the result ordering but clearly document any 
+    //assumptions in your code.  Please include unit tests. You can assume the entire 
+    //directory of doctors fits into memory.  Write in whatever language you're most 
+    //comfortable with. 
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get list of all doctors
+            ListOfDoctorsViewModel listObj = ListOfDoctorsViewModel.GetInstance();
+            List<DoctorViewModel> ListOfDoctors = listObj.ListOfDoctors;
+
+            // Display the list of available doctors 
+            ListOfDoctorsViewModel.DisplayListOfdoctors();
+
+            // Accept input doctor id for which similar doctors are to be searched
+            Console.Write("Please select User Id:");
+            int selectedDoctorId = Convert.ToInt32(Console.ReadLine());
+
+            // Accept Threshold Similarity Weight - This weight indicates the minimum similarity weight.
+            // We will be filtering out the doctors having similarity weight less than the threshold 
+            // similarity weight.
+            Console.Write("Please enter threshold similarity weight:");
+            int thresholdSimilarityWeight = Convert.ToInt32(Console.ReadLine());
+            
+            // Check if the selected doctor exists
+            var selectedDoctor = ListOfDoctors.FirstOrDefault(x => x.Id == selectedDoctorId);
+            ListOfDoctors = ListOfDoctors.Where(x => x.Id != selectedDoctorId).ToList();
+
+            // If exists proceed
+            if(selectedDoctor !=null)
+            {
+                // Compute the Similarity weights for all the doctors with respect to the selected doctor
+                selectedDoctor.ComputeSimilarityWeights();
+
+                // Sort the Similarity weights dictionary in descending order with respect to computed similarity 
+                // weights
+                // Filter out the entries having similarity weights lesser than the threshold similarity weight
+                selectedDoctor.SortAndFilterSimilarityWeights(thresholdSimilarityWeight);
+
+                // Display the output.
+                selectedDoctor.DisplayOutput();
+            }
+            else
+            {
+                Console.WriteLine("Please select valid entry");
+            }
+
+            Console.Read();
+
+        }
+    }
+}
